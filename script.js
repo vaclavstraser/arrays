1c57f4704c85c08964931ff2d2d1ab253ca9067b
baryska <barbora.skalova@newtontech.cz>
2022-03-09 00:05:46 +0100

    add basic files

---
+++ script.js
@@ -0,0 +1,42 @@
+const zelenina = [
+    'rajče',
+    'okurka',
+    'paprika',
+    'mrkev',
+    'brambory',
+    'celer',
+    'zelí',
+    'květák',
+    'ředkvičky',
+    'brokolice'
+]
+
+const ovoce = [
+    'jablko',
+    'hruška',
+    'jahody',
+    'třešně'
+]
+
+const vety = [
+   "Zůstaň stát",
+   "Přeskoč o dvě pole dopředu",
+   "Dej se doprava",
+   "Dej se doleva",
+   "Jdi rovně",
+   "Jedno kolo stojíš" 
+]
+
+
+// index
+// length
+// push/unshift/pop/shift
+// concat
+// indexOf
+// join
+// includes
+// sort
+// Math.random()
+
+
+
