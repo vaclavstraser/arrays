1b5c7d31d123ecde0012ef7c698d0eb34ef286a5
vašek <vaclav.cosials@seznam.cz>
2022-03-23 17:55:31 +0100

    generator

--- script.js
+++ script.js
@@ -1,42 +1,47 @@
 const zelenina = [
-    'rajče',
-    'okurka',
-    'paprika',
-    'mrkev',
-    'brambory',
-    'celer',
-    'zelí',
-    'květák',
-    'ředkvičky',
-    'brokolice'
-]
-
-const ovoce = [
-    'jablko',
-    'hruška',
-    'jahody',
-    'třešně'
-]
-
-const vety = [
-   "Zůstaň stát",
-   "Přeskoč o dvě pole dopředu",
-   "Dej se doprava",
-   "Dej se doleva",
-   "Jdi rovně",
-   "Jedno kolo stojíš" 
-]
-
-
-// index
-// length
-// push/unshift/pop/shift
-// concat
-// indexOf
-// join
-// includes
-// sort
-// Math.random()
-
-
-
+    "rajče",
+    "okurka",
+    "paprika",
+    "mrkev",
+    "brambory",
+    "celer",
+    "zelí",
+    "květák",
+    "ředkvičky",
+    "brokolice",
+  ];
+  
+  const ovoce = ["jablko", "hruška", "jahody", "třešně"];
+  
+  const zdravejidlo = zelenina.concat(ovoce);
+  
+  const cisla = [1, 5, 300, 3, 4];
+  
+  // console.log(Math.floor(Math.random() * 6) + 1);
+  
+  const kostka = document.querySelector("h3");
+  const tlacitko = document.querySelector("button");
+  tlacitko.onclick = () => {
+    const nahodneCislo = Math.floor(Math.random() * 6);
+    kostka.textContent = vety[nahodneCislo];
+  };
+  
+  const vety = [
+    "Zůstaň stát",
+    "Přeskoč o dvě pole dopředu",
+    "Dej se doprava",
+    "Dej se doleva",
+    "Jdi rovně",
+    "Jedno kolo stojíš",
+  ];
+  
+  
+  // index
+  // length
+  // push/unshift/pop/shift
+  // concat
+  // indexOf
+  // join
+  // includes
+  // sort
+  // Math.random() 
